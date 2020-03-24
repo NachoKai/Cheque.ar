888d7b27caf79de078f1f762e960ca3dc4795766
Nacho Caiafa <nachokai@outlook.com>
2020-03-24 18:07:05 -0300

    barra

--- src/main.js
+++ src/main.js
@@ -1,7 +1,25 @@
 let checkear = document.querySelector('#checkear');
+let checkeo = document.querySelector('#checkeo');
 
-checkear.addEventListener(click, clickCheck);
+checkear.addEventListener('click', clickCheck);
+checkeo.addEventListener('click', clickCheckBar);
 
 function clickCheck() {
-	console.log('Clickeaste nomas');
+	randomize();
+	checkeo.className = 'visible';
+}
+
+function clickCheckBar() {
+	randomize();
+	checkeo.className = 'oculto';
+}
+
+function randomize() {
+	$('.progressBar').each(function(index, progressBar) {
+		let percentage = $(progressBar).find('.percentage-check');
+		let progress = $(progressBar).find('.progress-check');
+		randNum = Math.floor(Math.random() * 100);
+		$(percentage).text(randNum + '%');
+		$(progress).width(randNum + '%');
+	});
 }
